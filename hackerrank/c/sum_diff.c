d3bbbc40365ec5e75bd20033164a0c6840f5c6e9
jaisai1108 <rajat1108sharma@gmail.com>
2022-06-25 14:02:15 +0530

    adding content to the file

---
+++ hackerrank/c/sum_diff.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j;
+    int l=0,k=0,c=0,d=0;
+    float a,b;
+    scanf("%d %d",i,j);
+    scanf("%f %f",a,b);
+    l=i+j;
+    k=i-j;
+    c=a+b;
+    d=a-b;
+    scanf("%d %d",&l,&k;
+    scanf("%0.1f %0.1f",&c,&d;
+    
+    
+    return 0;
+}
+
